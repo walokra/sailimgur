e2df3e20f8e023759f6d7b5b45233789c668c0d8
Marko Wallin <marko.wallin@iki.fi>
2014-12-21 22:42:36 +0200

    Fix Qt 5.2 WebView chokes on caches from older Qt versions.

--- main.cpp
+++ main.cpp
@@ -5,17 +5,47 @@
 #include <QGuiApplication>
 #include <QQmlContext>
 #include <QtQml>
+#include <QDebug>
 
 #include "src/imageuploader.h"
 #include "src/sailimgur.h"
+
+/**
+ * Clears the web cache, because Qt 5.2 WebView chokes on caches from older Qt versions.
+ */
+void clearWebCache() {
+    const QStringList cachePaths = QStandardPaths::standardLocations(
+                QStandardPaths::CacheLocation);
+
+    if (cachePaths.size()) {
+        // some very old versions of SailfishOS may not find this cache,
+        // but that's OK since they don't have the web cache bug anyway
+        const QString webCache = QDir(cachePaths.at(0)).filePath(".QtWebKit");
+        QDir cacheDir(webCache);
+        if (cacheDir.exists()) {
+            if (cacheDir.removeRecursively()) {
+                qDebug() << "Cleared web cache:" << webCache;
+            } else {
+                qDebug() << "Failed to clear web cache:" << webCache;
+            }
+        } else {
+            qDebug() << "Web cache does not exist:" << webCache;
+        }
+    } else {
+        qDebug() << "No web cache available.";
+    }
+}
 
 int main(int argc, char *argv[])
 {
 
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
+
+    clearWebCache();
+
     QScopedPointer<QQuickView> view(SailfishApp::createView());
 
-    app->setApplicationName("Sailimgur");
+    app->setApplicationName("harbour-sailimgur");
     app->setOrganizationName("harbour-sailimgur");
     app->setApplicationVersion(APP_VERSION);
 
